ca89e7c0cb590d9366f8697aa573fafbdccc415a
arcticfox321 <csrosecs321@gmail.com>
2025-12-21 15:36:19 +0300

    Update AracTurleri.cpp

--- AracTurleri.cpp
+++ AracTurleri.cpp
@@ -1,1 +1,25 @@
 #include "AracTurleri.h"
+
+// Otomobil
+Otomobil::Otomobil(std::string p)
+    : Arac(p, "Otomobil") {}
+
+double Otomobil::hesaplaUcret() const {
+    return 50.0; // örnek ücret
+}
+
+// Kamyonet
+Kamyonet::Kamyonet(std::string p)
+    : Arac(p, "Kamyonet") {}
+
+double Kamyonet::hesaplaUcret() const {
+    return 80.0;
+}
+
+// Motosiklet
+Motosiklet::Motosiklet(std::string p)
+    : Arac(p, "Motosiklet") {}
+
+double Motosiklet::hesaplaUcret() const {
+    return 30.0;
+}
