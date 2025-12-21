4d3b035da218f2d9a53d21f9169ed5317ccf2ed3
arcticfox321 <csrosecs321@gmail.com>
2025-12-21 15:51:12 +0300

    Update Arac.cpp

--- Arac.cpp
+++ Arac.cpp
@@ -1,33 +1,56 @@
 #include "Arac.h"
 
-// Constructor
+/*
+    Arac SINIFI
+    - Tüm araç türleri için temel (base) sınıftır
+    - Plaka, araç türü, giriş ve çıkış saatlerini tutar
+    - Polimorfizm için bilgileriYazdir() fonksiyonu kullanılır
+*/
+
+// ---------------- CONSTRUCTOR ----------------
+// Araç nesnesi oluşturulurken plaka ve tür atanır
+// Giriş saati otomatik olarak programın çalıştığı an alınır
 Arac::Arac(std::string p, std::string t)
     : plaka(p), tur(t) {
-    girisSaati = std::time(nullptr); // program çalıştığı an
+
+    // Aracın otoparka giriş zamanı (şu anki zaman)
+    girisSaati = std::time(nullptr);
+
+    // Çıkış saati başlangıçta 0 olarak ayarlanır
     cikisSaati = 0;
 }
 
-// Setter
+// ---------------- SETTER ----------------
+// Araç çıkışı yapıldığında çıkış saatini günceller
 void Arac::setCikisSaati(std::time_t saat) {
     cikisSaati = saat;
 }
 
-// Getter'lar
+// ---------------- GETTER'LAR ----------------
+
+// Aracın plaka bilgisini döndürür
 std::string Arac::getPlaka() const {
     return plaka;
 }
 
+// Aracın türünü döndürür (Otomobil, Kamyonet, Motosiklet vb.)
 std::string Arac::getTur() const {
     return tur;
 }
 
+// Aracın otoparka giriş saatini döndürür
 std::time_t Arac::getGirisSaati() const {
     return girisSaati;
 }
 
-// Bilgi yazdırma (polimorfik)
+// ---------------- BİLGİ YAZDIRMA ----------------
+// Sanal (virtual) fonksiyon olarak kullanılır
+// Türe bağlı olarak türetilmiş sınıflar tarafından ezilebilir
 void Arac::bilgileriYazdir() const {
+
+    // Araç bilgileri ekrana yazdırılır
     std::cout << "Plaka: " << plaka
               << " | Tür: " << tur
               << " | Giriş Saati: " << std::ctime(&girisSaati);
 }
+
