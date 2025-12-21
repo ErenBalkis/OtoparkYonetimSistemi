73d0f4955321a0d0993690156d671e2fea69a49e
arcticfox321 <csrosecs321@gmail.com>
2025-12-21 15:52:38 +0300

    Update Arac.h

--- Arac.h
+++ Arac.h
@@ -1,35 +1,75 @@
 #ifndef ARAC_H
 #define ARAC_H
 
-#include <string>
-#include <ctime>
-#include <iostream>
+#include <string>   // std::string için
+#include <ctime>    // time_t, time() fonksiyonları için
+#include <iostream> // cout kullanımı için
 
-// Soyut Sınıf (Abstract Class)
+/*
+    ARAC SINIFI (SOYUT SINIF)
+    --------------------------------
+    - Otopark sistemindeki tüm araç türleri için temel sınıftır
+    - Doğrudan nesnesi oluşturulamaz (abstract class)
+    - Polimorfizm için tasarlanmıştır
+*/
+
 class Arac {
 protected:
+    // Araca ait plaka bilgisi
     std::string plaka;
-    std::string tur; // "Otomobil", "Kamyonet" vb.
+
+    // Aracın türü (Otomobil, Kamyonet, Motosiklet vb.)
+    std::string tur;
+
+    // Aracın otoparka giriş zamanı
     std::time_t girisSaati;
+
+    // Aracın otoparktan çıkış zamanı
     std::time_t cikisSaati;
 
 public:
-    // Constructor
+    /*
+        CONSTRUCTOR
+        - Plaka ve araç türünü alır
+        - Giriş saati otomatik olarak atanır
+    */
     Arac(std::string p, std::string t);
 
-    // Virtual Destructor (Polimorfizm için şarttır)
+    /*
+        VIRTUAL DESTRUCTOR
+        - Polimorfik kullanımda bellek sızıntısını önler
+        - Türetilmiş sınıfların destructor'larının
+          doğru şekilde çağrılmasını sağlar
+    */
     virtual ~Arac() {}
 
-    // Saf Sanal Fonksiyon (Polimorfizm - Türetilen sınıflar bunu ezmek ZORUNDA)
-    virtual double hesaplaUcret() const = 0; 
+    /*
+        SAF SANAL FONKSİYON (PURE VIRTUAL)
+        - Her araç türü kendi ücret hesaplama mantığını
+          yazmak zorundadır
+        - Bu fonksiyon Arac sınıfını soyut (abstract) yapar
+    */
+    virtual double hesaplaUcret() const = 0;
 
-    // Getter & Setter metodları
+    // ---------------- GETTER & SETTER METOTLARI ----------------
+
+    // Aracın çıkış saatini ayarlar
     void setCikisSaati(std::time_t saat);
+
+    // Aracın plakasını döndürür
     std::string getPlaka() const;
+
+    // Aracın türünü döndürür
     std::string getTur() const;
+
+    // Aracın otoparka giriş saatini döndürür
     std::time_t getGirisSaati() const;
     
-    // Ekrana yazdırma yardımcısı
+    /*
+        Bilgi Yazdırma Fonksiyonu
+        - Sanal (virtual) olarak tanımlanmıştır
+        - Türetilmiş sınıflar isterse bu fonksiyonu ezebilir
+    */
     virtual void bilgileriYazdir() const;
 };
 
